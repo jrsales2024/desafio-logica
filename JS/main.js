f47e441cc915fb8871b61452b6191d681b995fbf
jrsales2024 <teofanessales.dp@gmail.com>
2024-10-19 20:26:20 -0300

    Meu primeiro commit

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+let posicaoInicial1 = parseInt(2);
+let totalPassos1 = parseInt(3);
+let posicaoInicial2 = parseInt(15);
+let totalPassos2 = parseInt(3);
+let posicaoInicial3 = parseInt(15);
+let totalPassos3 = parseInt(3);
+
+//TODO: Calcule a posição final do herói:
+posicaoFinal1 = posicaoInicial1 + totalPassos1;
+posicaoFinal2 = posicaoInicial2 + totalPassos2;
+posicaoFinal3 = posicaoInicial3 + totalPassos3;
+
+
+// Exemplo de uso:
+
+
+// Imprime a posição final
+console.log("A posição final do herói é:", posicaoFinal1);
+console.log("A posição final do herói é:", posicaoFinal2);
+console.log("A posição final do herói é:", posicaoFinal3);
---
+++ a.py
@@ -0,0 +1,5 @@
+a, b = 0, 1
+while a <=13:
+    if (a % 2) == 1:
+        print (a)
+    a, b = b, a+b
